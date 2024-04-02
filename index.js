4be1c39f3082063597cd45caedc8ff2ba6054e0f
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-04-02 12:00:23 +0300

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Defining the writeCards function
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let name of names) {
+        thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(num) {
+    // Checking if the input is a positive integer
+    if (num <= 0 || !Number.isInteger(num)) {
+        console.log("Please provide a positive integer greater than zero.");
+        return;
+    }
+
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+
+
